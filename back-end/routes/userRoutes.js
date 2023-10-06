cb14523d9f66fd9cdb92ba2dbb8975f3436adf39
devAtefturki <turkiatef2@gmail.com>
2023-10-06 15:26:30 +0100

    added routes for all previous methods

---
+++ back-end/routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express= require ('express')
+const router=express.Router()
+const {register, getUser, login, logout, verifyCode, modifyPass}=require("../controller/users")
+
+
+
+router.post('/register',register)
+router.post('/verify',verifyCode)
+router.post('login',login)
+router.get('logout',logout)
+router.get('getUser',getUser)
+router.put('updatePass',modifyPass)
+
+
+module.exports =router
