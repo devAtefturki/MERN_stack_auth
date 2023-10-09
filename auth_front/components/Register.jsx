d569a79317ef94d440d47804a9d273a3158b3fcb
devAtefturki <turkiatef2@gmail.com>
2023-10-09 15:52:00 +0100

    added some syling in registration

--- auth_front/components/Register.jsx
+++ auth_front/components/Register.jsx
@@ -13,21 +13,23 @@ const MAX_LENGTH=15,
 MIN_LENGTH=8,
 PASS_LABELS=["Too Short","Weak", "Normal","Strong","Secure"];
     return (
-        <View>
-            <Text>Registration</Text>
-            <View>
+        <View style={styles.container}>
+            <Text style={styles.logo}>Registration</Text>
+            <View style={styles.inputView}>
                 <TextInput 
+                style={styles.inputText}
                 placeHolder='username...'
                 onChangeText={text=>setCredentials({...credentials,username:text})}/>
             </View>
-            <View>
-                <TextInput
+            <View style={styles.inputView}>
+                <TextInput style={styles.inputText}
                 placeholder='Email...'
                 onChangeText={text=>setCredentials({...credentials,useremail:text})}/>
             </View>
-            <View>
+            <View style={styles.inputView}>
                 <TextInput
                 secureTextEntry
+                style={styles.inputView}
                 placeholder="Password..."
                 onChangeText={text=>setCredentials({...credentials,password:text})}/>
             </View>
@@ -38,7 +40,7 @@ PASS_LABELS=["Too Short","Weak", "Normal","Strong","Secure"];
                 placeholder="Retype Password To Confirm..."
                 onChangeText={text=>setCheck(text)}/>
             </View>
-            <TouchableOpacity onPress={
+        #<TouchableOpacity onPress={
                 ()=>{
                     if (check!==credentials.password){
                         return Alert.alert("password does not match")
@@ -56,9 +58,54 @@ PASS_LABELS=["Too Short","Weak", "Normal","Strong","Secure"];
 
             }
             >
-                <Text>SIGNUP</Text>
+                <Text style={styles.loginText}>SIGNUP</Text>
             </TouchableOpacity>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo: {
+      fontWeight: "bold",
+      fontSize: 50,
+      color: "#3CB371",
+      marginBottom: 40
+    },
+    inputView: {
+      width: "80%",
+      backgroundColor: "#fff",
+      borderBottomWidth:1,
+      borderRadius: 25,
+      height: 50,
+      marginBottom: 20,
+      justifyContent: "center",
+      padding: 20
+    },
+    inputText: {
+      height: 50,
+      color: "black"
+    },
+    forgot: {
+      color: "white",
+      fontSize: 11
+    },
+    loginBtn: {
+      width: "80%",
+      backgroundColor: "#3CB371",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      marginBottom: 10
+    },
+    loginText: {
+      color: "white"
+    }
+  });
 export default Register
